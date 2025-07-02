29d79c38fa71b105ca5f7e240a1492a6df74ddd5
Rerey <reyvars155@gmail.com>
2025-07-02 19:01:35 +0700

    oke

--- database/seeders/CustompackageSeeder.php
+++ database/seeders/CustompackageSeeder.php
@@ -11,23 +11,23 @@ class CustompackageSeeder extends Seeder
     {
         Custompackage::create([
             'name' => 'Custom 1',
-            'price' => 250000,
+            'price' => 600000,
             'category' => 'Media Sosial',
-            'description' => 'Konten Bebas, Konsultasi Gratis',
+            'description' => 'Kalender Konten, 8 Feed(foto), 3 Reels(max 30 sec), 4 Stories(foto),1 Platform Media Sosial ',
         ]);
 
         Custompackage::create([
-            'name' => 'Custom Paket B',
-            'price' => 350000,
-            'category' => 'Iklan Digital',
-            'description' => 'Termasuk copywriting dan desain iklan',
+           'name' => 'Custom 2',
+            'price' => 1100000,
+            'category' => 'Media Sosial',
+            'description' => 'Kalender Konten, 14 Feed(foto), 6 Reels(max 30 sec), 4 Stories(foto),2 Platform Media Sosial ',
         ]);
 
         Custompackage::create([
-            'name' => 'Custom Paket C',
-            'price' => 450000,
-            'category' => 'Branding',
-            'description' => 'Desain logo, warna brand, dan template konten',
+             'name' => 'Custom 3',
+            'price' => 1700000,
+            'category' => 'Media Sosial',
+            'description' => 'Kalender Konten, 16 Feed(foto), 6 Reels(max 30 sec), 6 Stories(foto),2 Platform Media Sosial ',
         ]);
     }
 }
